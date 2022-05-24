7779c74fd66ef4e528058eb1879b61d855f35559
jhechus <jess.osm1412@gmail.com>
2022-05-23 22:07:51 -0500

    agregando los inicios de php y el servidor de prepos con XAMPP

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Ecommerce prof </title>
+</head>
+<body>
+    <h1> este es h1 </h1>
+    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt mollitia incidunt nihil natus, dolore aspernatur amet atque perferendis, ipsum recusandae impedit obcaecati provident eaque exercitationem sapiente pariatur ipsam quam in?
+    </p>
+    <a href="alo"> nose que poner </a>
+
+</body>
+</html>
