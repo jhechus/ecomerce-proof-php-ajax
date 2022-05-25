86225193a15d0ca1ea4eda4afea13843238bc407
jhechus <jess.osm1412@gmail.com>
2022-05-24 20:49:08 -0500

    agregando waitMe, jquery y alert

--- index.php
+++ index.php
@@ -1,16 +1,52 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> Ecommerce prof </title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title> Ecommerce prof </title>
+ 
+  <!--boostrap-->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+ <!--fontawosame--> 
+ <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous"/>
+ 
+<!-- wait Me -->
+<link rel="stylesheet" href="assets/plugins/waitme/waitMe.min.css">
+
 </head>
+
 <body>
-    <h1> este es h1 </h1>
-    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt mollitia incidunt nihil natus, dolore aspernatur amet atque perferendis, ipsum recusandae impedit obcaecati provident eaque exercitationem sapiente pariatur ipsam quam in?
-    </p>
-    <a href="alo"> nose que poner </a>
+  <h1> este es h1 </h1>
+  <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt mollitia incidunt nihil natus, dolore aspernatur amet atque perferendis, ipsum recusandae impedit obcaecati provident eaque exercitationem sapiente pariatur ipsam quam in?
+  </p>
+  <a href="alo"> nose que poner </a>
+
+
+<!-- jquery -->
+<script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+
+  <!-- JavaScript Bundle with Popper (boostrap) -->
+<script 
+src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
+</script>
+
+<!-- sweet alert 2 -->
+<script 
+src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<!-- <script>
+  Swal.fire("hola", "cogemos? OwO", "success");
+</script> -->
+
+<!-- waitMe -->
+<script 
+src="assets/plugins/waitme/waitMe.min.js"></script>
+
+<script>
+   $('body').waitMe('hide');
+</script>
+
 
 </body>
 </html>
