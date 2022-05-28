c1598afb95f60957ebf030bc13b4eb78d1a70f3e
jhechus <jess.osm1412@gmail.com>
2022-05-27 21:58:15 -0500

    Agregando maquetacion del carrito

--- index.php
+++ index.php
@@ -53,7 +53,7 @@
       <h1> Productos </h1>
       <div class="row">
         
-        <div class="col-3">
+        <div class="col-3 mb-3">
           <div class="card">
             <img src="https://placeimg.com/300/300/tech" alt="Producto" class="card-img-top">
             <div class="card-body text-right p-2">
@@ -63,7 +63,7 @@
           </div>
         </div>
 
-        <div class="col-3">
+        <div class="col-3 mb-3">
           <div class="card">
             <img src="https://placeimg.com/300/300/tech" alt="Producto" class="card-img-top">
             <div class="card-body text-right p-2">
@@ -73,7 +73,7 @@
           </div>
         </div>
 
-        <div class="col-3">
+        <div class="col-3 mb-3">
           <div class="card">
             <img src="https://placeimg.com/300/300/tech" alt="Producto" class="card-img-top">
             <div class="card-body text-right p-2">
@@ -83,7 +83,27 @@
           </div>
         </div>
 
-        <div class="col-3">
+        <div class="col-3 mb-3">
+          <div class="card">
+            <img src="https://placeimg.com/300/300/tech" alt="Producto" class="card-img-top">
+            <div class="card-body text-right p-2">
+              <h5 class="card-title text-truncate"> Texto del producto Lorem ipsum dolor </h5>
+              <button class="btn btn-sm btn-success" data-toggle="tooltip" title="Agregar al carrito"><i class="fas fa-plus"></i> Agregar al carrito </button>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-3 mb-3">
+          <div class="card">
+            <img src="https://placeimg.com/300/300/tech" alt="Producto" class="card-img-top">
+            <div class="card-body text-right p-2">
+              <h5 class="card-title text-truncate"> Texto del producto Lorem ipsum dolor </h5>
+              <button class="btn btn-sm btn-success" data-toggle="tooltip" title="Agregar al carrito"><i class="fas fa-plus"></i> Agregar al carrito </button>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-3 mb-3">
           <div class="card">
             <img src="https://placeimg.com/300/300/tech" alt="Producto" class="card-img-top">
             <div class="card-body text-right p-2">
@@ -97,8 +117,75 @@
     </div>
 
     <!-- carrito -->
-    <div class="col-xl-4">
+    <div class="col-xl-4 bg-light">
     <h1> Carrito </h1>
+    <!-- cart content -->
+    <div class="table-responsive">
+      <table class="table table-hover table-border table-sm">
+        <thead>
+          <tr>
+            <th> Producto </th>
+            <th class="text-center"> Cantidad </th>
+            <th class="text-center"> Total </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="align-middle"> Producto 1
+              <small class="d-block text-muted"> Te de hierbabuena </small>
+            </td>
+            <td class="align-middle text-center" width="5%"> 
+              <input type="number" class="form-control form-control-sm" min="0" max="40" value="1"> </td>
+            <td class="align-middle text-center"> $120 </td>
+          </tr>
+
+          <tr>
+            <td class="align-middle"> Producto 2
+              <small class="d-block text-muted"> Amor anillo </small>
+            </td>
+            <td class="align-middle text-center" width="5%"> 
+              <input type="number" class="form-control form-control-sm" min="0" max="40" value="1"> </td>
+            <td class="align-middle text-center"> $60 </td>
+          </tr>
+
+          <tr>
+            <td class="align-middle"> Producto 3
+              <small class="d-block text-muted"> celular </small>
+            </td>
+            <td class="align-middle text-center" width="5%"> 
+              <input type="number" class="form-control form-control-sm" min="0" max="40" value="1"> </td>
+            <td class="align-middle text-center"> $180 </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <button class="btn btn-sm btn-danger"> Vaciar carrito </button>
+    <!-- end cart content -->
+<br><br>
+    <!-- cart total -->
+    <table class="table">
+      <tr>
+        <th class="border-0">Subtotal
+          <td class="text-success text-right border-0">$250</td>
+        </th>
+      </tr>
+      <tr>
+        <th>Envio
+          <td class="text-success text-right">$50</td>
+        </th>
+      </tr>
+      <tr>
+        <th>Total
+          <td class="text-success text-right"> <h3 class="font-weight-bold"> $300 </h3></td>
+        </th>
+      </tr>
+    </table>
+    <!-- end cart total -->
+
+    <!-- pay now button -->
+    <button class="btn btn-info btn-lg btn-block"> Pagar ahora </button>
+    <!-- end pay now button -->
+
     </div>
   </div>
 </div>
