d9dcf4d4d7be28de27f6418a7728fc56b9d2c4c3
jhechus <jess.osm1412@gmail.com>
2022-06-01 22:05:48 -0500

    agregando imagenes y archivo products.php

---
+++ app/product.php
@@ -0,0 +1,74 @@
+<?php
+
+//id
+//sku
+//name
+//price
+//imagen
+
+$products = [
+[
+    'id' => 1,
+    'sku' => '345678',
+    'name' =>  'tenis soldado',
+    'imagen' => 'soldado.png',
+    'price' => 799.99
+],
+[
+    'id' => 2,
+    'sku' => '346578',
+    'name' =>  'tenis hello kitty',
+    'imagen' => 'hello_kitty.png',
+    'price' => 799.99
+],
+[
+    'id' => 3,
+    'sku' => '345458',
+    'name' =>  'tenis dia de muertos',
+    'imagen' => 'dia_de_muertos.png',
+    'price' => 999.99
+],
+[
+    'id' => 4,
+    'sku' => '545571',
+    'name' =>  'tenis chuck taylor mujer',
+    'imagen' => 'chuck_taylor_mujer.png',
+    'price' => 899.99
+],
+[
+    'id' => 5,
+    'sku' => '546678',
+    'name' =>  'tenis chuck taylor',
+    'imagen' => 'chuck_taylor',
+    'price' => 899.99
+],
+[
+    'id' => 6,
+    'sku' => '384278',
+    'name' =>  'tenis chuch taylor juveniles',
+    'imagen' => 'chuch_taylor_juveniles.png',
+    'price' => 899.99
+],
+[
+    'id' => 7,
+    'sku' => '734678',
+    'name' =>  'tenis casual hombre',
+    'imagen' => 'casual_hombre.png',
+    'price' => 599.99
+],
+[
+    'id' => 8,
+    'sku' => '349467',
+    'name' =>  'tenis casuales niña',
+    'imagen' => 'casuales_niña.png',
+    'price' => 599.99
+],
+[
+    'id' => 9,
+    'sku' => '392576',
+    'name' =>  'tenis batman',
+    'imagen' => 'batman.png',
+    'price' => 899.99
+],
+
+];
