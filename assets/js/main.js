f985de744a59b32d51b8aaf02d7ead75af5b11ce
jhechus <jess.osm1412@gmail.com>
2022-06-14 20:30:18 -0500

    se resuelven los problemas que se tenian pero no se ve la vista como en la rama

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,16 @@
-$(document).ready(function() {
-   
-    function load_cart() {
-        var wrapper = $('#cartwrapper'),
-        action = 'get';
+$( document ).ready(function() {
+    
+    //cargar el carro
+    function load_cart(){
+        //se declara variable para poder trabajar mas comodamente
+    var wrapper = $('#cart_wrapper'),
+    action = 'get';
 
-        wrapper = waitMe();
+        //wrapper es la funcion mas facil que esta en el contenido
+        //con el wait se pone la opcion de cargando
+    wrapper.waitMe('hide');
     };
     load_cart();
+
 
 });
