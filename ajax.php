7ed1159e45ccaa12cffb6ef92d218d803a9ee60d
jhechus <jess.osm1412@gmail.com>
2022-06-23 14:26:51 -0500

    se desactiva el boton de compras para cuando no haya nada

--- ajax.php
+++ ajax.php
@@ -74,24 +74,24 @@ switch ($action) {
       <table class="table">
         <tr>
           <th class="border-0">Subtotal
-            <td class="text-success text-right border-0">$250</td>
+            <td class="text-success text-right border-0">' .format_currency($cart['subtotal']). '</td>
           </th>
         </tr>
         <tr>
           <th>Envio
-            <td class="text-success text-right"> $ <?php echo SHIPPING_COST; ?></td>
+            <td class="text-success text-right">' .format_currency($cart['shipment']). '</td>
           </th>
         </tr>
         <tr>
           <th>Total
-            <td class="text-success text-right"> <h3 class="font-weight-bold"> $300 </h3></td>
+            <td class="text-success text-right"> <h3 class="font-weight-bold">' .format_currency($cart['total']). '</h3></td>
           </th>
         </tr>
       </table>
       <!-- end cart total -->
 
       <!-- pay now button -->
-      <button class="btn btn-info btn-lg btn-block"> Pagar ahora </button>
+      <button class="btn btn-info btn-lg btn-block" disabled> Pagar ahora </button>
       <!-- end pay now button -->';
         json_output(200, 'OK', $output);
         break;
